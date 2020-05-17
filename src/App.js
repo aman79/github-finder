6b0c9c30ac0bc3c2d697f8d49ff39300ed002378
Aman Gupta <aman.gupta0107@gmail.com>
2020-05-17 23:26:26 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 		this.setState({ loading: true });
 		const res = await axios.get('https://api.github.com/users');
 		console.log(res);
-		this.setState({ users: res.data });
+		this.setState({ users: res.data, loading: false });
 	}
 
 	render() {
